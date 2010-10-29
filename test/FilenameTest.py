0c6f3618ecdd6eace828f22d94483d5cdfb48d6f
Mattia Barbon <mattia@develer.com>
2010-10-29 12:21:49 +0000

    Some more tests for command quoting.

--- test/FilenameTest.py
+++ test/FilenameTest.py
@@ -9,12 +9,13 @@ class TestStrangeFilenameSvn(TestDLI.TestDLI):
     template_path = 'test/repository/special.svn.dump'
 
     def testStrange(self):
-        self.assertEqualDiff(self.runDli(start_revision=1, end_revision=2,
+        self.assertEqualDiff(self.runDli(start_revision=1, end_revision=3,
                                          show_text=True, show_html=False),
                              """
 ChangeSet Index:
 
 CS1 [#cs1] - Added a file with a funky name.
+CS2 [#cs2] - Sorry, forgot some.
 
 Added a file with a funky name.
 
@@ -25,6 +26,11 @@ A      2  foo-(1)#&$f.txt
 +++ foo-(1)#&$f.txt	(revision 2)
 +Strange name...
 
+
+Sorry, forgot some.
+
+D      2  foo-(1)#&$f.txt
+M      3  foo: space,	tab<|.txt
 
 
 """)
@@ -37,12 +43,13 @@ class TestStrangeFilenameGit(TestDLI.TestDLI):
     template_path = 'test/repository/special.git.dump'
 
     def testStrange(self):
-        self.assertEqualDiff(self.runDli(start_revision='760b618f', end_revision='4bb7a4bb',
+        self.assertEqualDiff(self.runDli(start_revision='760b618f', end_revision='d480cff7',
                                          show_text=True, show_html=False),
                              """
 ChangeSet Index:
 
 CS1 [#cs1] - Added a file with a funky name.
+CS2 [#cs2] - Sorry, forgot some.
 
 Added a file with a funky name.
 
@@ -54,6 +61,11 @@ A 4bb7a4bbfcd702e86e822d9998a4e575a49db81e  foo-(1)#&$f.txt
 @@ -0,0 +1 @@
 +Strange name...
 
+
+Sorry, forgot some.
+
+M d480cff78fa1af01b0a62875e2a2181607bd9f88  "foo: space,\\ttab<|.txt"
+D 4bb7a4bbfcd702e86e822d9998a4e575a49db81e  foo-(1)#&$f.txt
 
 
 """)
