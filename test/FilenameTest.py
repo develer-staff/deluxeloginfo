c58b3a29e0acfcc555af846f3dec733866a3a063
Mattia Barbon <mattia@develer.com>
2010-10-29 11:01:40 +0000

    Quote filenames passed to Git when passing through the shell.

--- test/FilenameTest.py
+++ test/FilenameTest.py
@@ -8,7 +8,7 @@ class TestStrangeFilenameSvn(TestDLI.TestDLI):
     repository_name = 'special.svn'
     template_path = 'test/repository/special.svn.dump'
 
-    def testUTF(self):
+    def testStrange(self):
         self.assertEqualDiff(self.runDli(start_revision=1, end_revision=2,
                                          show_text=True, show_html=False),
                              """
@@ -28,6 +28,36 @@ A      2  foo-(1)#&$f.txt
 
 
 """)
+
+
+class TestStrangeFilenameGit(TestDLI.TestDLI):
+    repository_name = 'special.git'
+    repository_type = 'git'
+    repository_module = 'git' # to test that it is ignored
+    template_path = 'test/repository/special.git.dump'
+
+    def testStrange(self):
+        self.assertEqualDiff(self.runDli(start_revision='760b618f', end_revision='4bb7a4bb',
+                                         show_text=True, show_html=False),
+                             """
+ChangeSet Index:
+
+CS1 [#cs1] - Added a file with a funky name.
+
+Added a file with a funky name.
+
+A 4bb7a4bbfcd702e86e822d9998a4e575a49db81e  foo-(1)#&$f.txt
+
+======================================================================
+--- /dev/null
++++ b/foo-(1)#&$f.txt
+@@ -0,0 +1 @@
++Strange name...
+
+
+
+""")
+
 
 if __name__ == '__main__':
     unittest.main()
