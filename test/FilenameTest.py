211d4f91c159d417b2bb9bb23b57f7540125a528
Mattia Barbon <mattia@develer.com>
2010-10-29 14:29:48 +0000

    Add Mercurial test for files with strange characters (spaces od not work yet).

--- test/FilenameTest.py
+++ test/FilenameTest.py
@@ -71,5 +71,34 @@ D 4bb7a4bbfcd702e86e822d9998a4e575a49db81e  foo-(1)#&$f.txt
 """)
 
 
+class TestStrangeFilenameHg(TestDLI.TestDLI):
+    repository_name = 'special.hg'
+    repository_type = 'hg'
+    template_path = 'test/repository/special.hg.dump'
+
+    def testStrange(self):
+        self.assertEqualDiff(self.runDli(start_revision='aafaa78a', end_revision='aafaa78a',
+                                         show_text=True, show_html=False),
+                             """
+ChangeSet Index:
+
+CS1 [#cs1] - Added a file with a funky name.
+
+Added a file with a funky name.
+
+
+A aafaa78a4d75569da8bfac3c0cfc1ff2ad08a68b  foo-(1)#&$f.txt
+
+======================================================================
+--- /dev/null	Thu Jan 01 00:00:00 1970 +0000
++++ b/foo-(1)#&$f.txt	Fri Oct 29 14:40:12 2010 +0200
+@@ -0,0 +1,1 @@
++Strange name...
+
+
+
+""")
+
+
 if __name__ == '__main__':
     unittest.main()
